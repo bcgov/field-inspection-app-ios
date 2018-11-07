86d71fe66eea3c6a63202c5eb7f3b8bff9ca52b7
Jason C. Leach <jason.leach@fullboar.ca>
2018-11-07 10:40:57 -0800

    Add them and FA support

---
+++ EAO/FAFormatter.swift
@@ -0,0 +1,9 @@
+//
+//  FAFormatter.swift
+//  Field Insp
+//
+//  Created by Jason Leach on 2018-11-06.
+//  Copyright © 2018 FreshWorks. All rights reserved.
+//
+
+import Foundation
---
+++ EAO/Theme.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  EAO
+//
+//  Created by Jason Leach on 2018-11-07.
+//  Copyright © 2018 Province of British Columbia. All rights reserved.
+//
+
+import Foundation
