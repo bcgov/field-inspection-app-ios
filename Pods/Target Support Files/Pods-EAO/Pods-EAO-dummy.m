e53da43ac0861e2037d62b6f49a84af7ad1ea24f
Jason C. Leach <jason.leach@fullboar.ca>
2018-11-09 10:17:29 -0800

    Remove Pods directory

--- Pods/Target Support Files/Pods-EAO/Pods-EAO-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EAO : NSObject
-@end
-@implementation PodsDummy_Pods_EAO
-@end
--- Pods/Target Support Files/Pods-EAOUITests/Pods-EAOUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EAOUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EAOUITests
-@end
